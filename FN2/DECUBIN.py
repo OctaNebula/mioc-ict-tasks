6d45133fbb5af713ddd90a56c01322510456463f
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-04-25 15:45:30 +0200

    Added functions 2

---
+++ FN2/DECUBIN.py
@@ -0,0 +1,1 @@
+def bintodec(number): return int(str(number), 2)
---
+++ FN2/KVADRAT.py
@@ -0,0 +1,6 @@
+def kvadrat(number):
+    return number**2
+numberofnumbers = int(input())
+for i in range(numberofnumbers):
+    x = int(input())
+    print(kvadrat(x))
