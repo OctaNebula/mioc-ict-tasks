b07964fd69edfd331bfeff06c47261bfdfe1af0b
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-05-09 15:37:39 +0200

    Added explanation to KEMIJA

--- STR4/KEMIJA.py
+++ STR4/KEMIJA.py
@@ -1,5 +1,8 @@
 encodedstring = input()
+
+#As long as the string contains "apa", "epe", "ipi", "opo", or "upu" commit to the loop
 while encodedstring.__contains__("apa") or encodedstring.__contains__("epe") or encodedstring.__contains__("ipi") or encodedstring.__contains__("opo") or encodedstring.__contains__("upu"):
+    #Replaces "apa", "epe", "ipi", "opo", "upu" with their respective vowels
     encodedstring = encodedstring.replace("apa", "a")
     encodedstring = encodedstring.replace("epe", "e")
     encodedstring = encodedstring.replace("ipi", "i")
