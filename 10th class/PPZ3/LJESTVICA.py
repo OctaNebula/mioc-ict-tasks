c4bca52450bc794be57f8cdc5f5bd0bef43ef2c9
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-05-18 13:56:39 +0200

    bruh

--- 10th class/PPZ3/LJESTVICA.py
+++ 10th class/PPZ3/LJESTVICA.py
@@ -6,8 +6,8 @@ string = input()
 stringoriginal = string
 string = string.split("|")
 #nukes everything after the first note
-for i in string:
-    i = i[0]
+for i in range(len(string)):
+    string[i] = string[i][0]
 string = "".join(string)
 #counts the ammount of major notes for each string
 for i in string:
@@ -15,7 +15,8 @@ for i in string:
         cmajorammount += 1
     elif i in aminormainnotes:
         cminorammount += 1
-
+print(cmajorammount, cminorammount)
+print(string)
 if cmajorammount > cminorammount:
     print("C-dur")
 elif cminorammount > cmajorammount:
