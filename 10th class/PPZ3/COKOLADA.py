ceebf5f4fa119aeb8cd7acdcd8434b21a284e128
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-05-18 14:28:32 +0200

    test

---
+++ 10th class/PPZ3/COKOLADA.py
@@ -0,0 +1,12 @@
+chocox, chocoy = map(int, input().split())
+for i in range(3):
+    string = input()
+    string = string.split()
+    #2nd member of the list is either "redova" or "stupaca", redova is rows, stupaca is columns
+    #1st is the ammount of rows/columns
+    if string[1] == "redova":
+        print(chocox)
+        chocox -= int(string[0])
+    elif string[1] == "stupaca":
+        print(chocoy)
+        chocoy -= int(string[0])
