f3dcfc6c9fa81e36df91572e9c85ecfb47fcffa0
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-09-14 14:33:32 +0200

    fixed an oversight in TEST2.py

--- 11th class/MTX0/TEST2.py
+++ 11th class/MTX0/TEST2.py
@@ -1,5 +1,7 @@
 matrix = []
 for i in matrix:
-    if max(i) >= 10:
-        #prints the position of the cell in the matrix
-        print(matrix.index(i), i.index(max(i)))
+    for j in i:
+        if j >= 10:
+            #prints the position of the cell in the matrix
+            print(matrix.index(i), i.index(j))
+
