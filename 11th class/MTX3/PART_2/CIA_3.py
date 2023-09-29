8f84c6c49a2d76caeeb3995853126814f4c6a3fc
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-09-29 15:36:31 +0200

    fixed CIA_3 and added a .py extension to CIA_1

--- 11th class/MTX3/PART_2/CIA_3.py
+++ 11th class/MTX3/PART_2/CIA_3.py
@@ -1,22 +1,22 @@
 #inputs the matrix1
-r1, c1 = map(int, input().split())
+r, c = map(int, input().split())
 matrix1 = []
-for i in range(r1):
+for i in range(r):
     matrix1.append(list(map(int, input().split())))
 
 #inputs the matrix2
-r2, c2 = map(int, input().split())
+
 matrix2 = []
-for i in range(r2):
+for i in range(r):
     matrix2.append(list(map(int, input().split())))
 
 #replaces the every third column of matrix1 with matrix2, starting from 0th column (actually first becasue of 0 indexing)
-for i in range(0, c1, 3):
-    for j in range(r1):
+for i in range(0, c, 3):
+    for j in range(r):
         matrix1[j][i] = matrix2[j][i]
     
 #prints matrix1
-for i in range(r1):
-    for j in range(c1):
+for i in range(r):
+    for j in range(c):
         print(matrix1[i][j], end=" ")
     print()
