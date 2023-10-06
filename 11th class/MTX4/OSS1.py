9e6afb52a212182957241f1466cadce4ac309c92
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-10-06 14:18:51 +0200

    Solved another task

---
+++ 11th class/MTX4/OSS1.py
@@ -0,0 +1,16 @@
+matrix = []
+for i in range(8):
+    matrix.append(list(map(int, input().split())))
+
+#prints the row number (1st is 1 not 0) where the biggest number is
+
+max = 0
+row = 0
+
+for i in range(8):
+    for j in range(8):
+        if matrix[i][j] > max:
+            max = matrix[i][j]
+            row = i+1
+        
+print(row)
