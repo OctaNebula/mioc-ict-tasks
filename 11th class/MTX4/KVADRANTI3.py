b79e8ae6ad3bff21b2ac63b753dc33668d4f0f9a
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-10-06 14:14:46 +0200

    solved another task

---
+++ 11th class/MTX4/KVADRANTI3.py
@@ -0,0 +1,20 @@
+r = int(input())
+c = r
+matrix = []
+for i in range(r):
+    matrix.append(list(map(int, input().split())))
+
+white = 0
+black = 0
+
+#the matrix is basically a checkerboard; it sums the numbers in the white and black squares
+for i in range(r):
+    for j in range(c):
+        if (i + j) % 2 == 0:
+            white += matrix[i][j]
+        else:
+            black += matrix[i][j]
+
+
+#prints the result
+print("A: {halfA} B: {halfB}".format(halfA=white, halfB=black))
