79646b748412ab90f0e121f79d1acf4e7c1b3817
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2024-05-15 09:05:08 +0200

    Solved CAESAR.py

---
+++ 11th class/CTG0/CAESAR.py
@@ -0,0 +1,30 @@
+commonBigrams = ["TH", "HE", "IN", "ER", "AN", "RE"]
+
+def caesar_encrypt(string, shift):
+    encrypted_string = ""
+    string = string.upper()
+    for char in string:
+        if char.isalpha():
+            ascii_val = ord(char) + shift
+            if ascii_val > ord("Z"):
+                ascii_val -= 26
+            elif ascii_val < ord("A"):
+                ascii_val += 26
+            encrypted_string += chr(ascii_val)
+        else:
+            encrypted_string += char
+    return encrypted_string
+
+def caesar_decrypt(string, bigrams, reqScore):
+    score = 0
+    string = string.upper()
+    for shift in range(26):
+        decrypted_string = caesar_encrypt(string, -shift)
+        for bigram in bigrams:
+            score += decrypted_string.count(bigram)
+        if score >= reqScore:
+            return decrypted_string
+    return "Decryption failed"
+
+def caesar_decrypt_knownKey(string, shift):
+    return caesar_encrypt(string, -shift)
--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Here's a small list of what the abbreviations mean:
 - `REC` - Recursion
 - `DC` - Dictionaries
 - `FL` - Files
+- `CTG` - Cryptography
 
 There's also the omnipresence of me naming tasks "`Zx`", where x is a number *("Zadatak", translated from croatian, task)*. That's for when we get tasks which don't really have any name (such as tasks we get on work papers, which are usually simply numerated), so i just name them `Z1`, `Z2`, `Z3`, etc.
 
