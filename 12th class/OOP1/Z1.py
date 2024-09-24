3f1e06cc8f2d7b77e8684f88ec0de6906ae78326
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2024-09-24 19:01:41 +0200

    added line plotting via matplotlib

--- 12th class/OOP1/Z1.py
+++ 12th class/OOP1/Z1.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 
 class Point:
     def __init__(self, x, y):
@@ -8,9 +9,18 @@ class Point:
     def distance(self, p):
         return math.sqrt((self.x - p.x) ** 2 + (self.y - p.y) ** 2)
 
+    def plot(self):
+        plt.plot(self.x, self.y, 'ro')
+        plt.axis([0, 10, 0, 10])
+        plt.show()
 
+    def plotArray(self, p):
+        plt.plot([self.x, p.x], [self.y, p.y], 'ro-')
+        plt.axis([0, 10, 0, 10])
+        plt.show()
 p1 = Point(1, 2)
 p2 = Point(4, 6)
 
 print(p1.distance(p2))
 
+p1.plotArray(p2)
