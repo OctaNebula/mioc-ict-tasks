7c16967d6346dafffee91806ac155b226050774d
Dario Vuksan <65848142+IamMusavaRibica@users.noreply.github.com>
2024-11-05 10:28:16 +0100

    fix typo

--- 11th class/DC4/Z2.py
+++ 11th class/DC4/Z2.py
@@ -1,6 +1,6 @@
-p1lenght = int(input()) # useless variable but the task requires it so yea
+p1length = int(input()) # useless variable but the task requires it so yea
 p1 = list(map(int, input().split()))
-p2lenght = int(input()) # same here lol
+p2length = int(input()) # same here lol
 p2 = list(map(int, input().split()))
 p1 = {i: p1[i] for i in range(len(p1))}
 p2 = {i: p2[i] for i in range(len(p2))}
