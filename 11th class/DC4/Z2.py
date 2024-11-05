b504d0809a86083482cbb13eb07cae741e0018cd
Dario Vuksan <65848142+IamMusavaRibica@users.noreply.github.com>
2024-11-05 10:30:22 +0100

    add alternate solution

--- 11th class/DC4/Z2.py
+++ 11th class/DC4/Z2.py
@@ -15,3 +15,12 @@ for i in p1:
 
 print(' '.join(map(str, result.keys())))
 print()
+
+# Alternate solution (lines 8-14):
+
+# from collections import defaultdict
+#
+# result = defaultdict(int)
+# for i in p1:
+#     for j in p2:
+#         result[i+j] += p1[i] * p2[j]
