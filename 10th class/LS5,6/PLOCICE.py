8c91c3bb55039a65a7cb0ab8fcbfd1d48d8ebd7a
Dario Vuksan <65848142+IamMusavaRibica@users.noreply.github.com>
2024-11-05 10:36:25 +0100

    Update PLOCICE.py

--- 10th class/LS5,6/PLOCICE.py
+++ 10th class/LS5,6/PLOCICE.py
@@ -1,5 +1,5 @@
 plocice = []
-sortedplocice = [1, 2 ,3 ,4 ,5]
+sortedplocice = [1, 2, 3, 4, 5]
 first, second, third, fourth, fifth = map(int, input().split())
 plocice.append(first)
 plocice.append(second)
@@ -10,16 +10,16 @@ while plocice != sortedplocice:
     #if the first plocica is bigger than the second one, swap them
     if plocice[0] > plocice[1]:
         plocice[0], plocice[1] = plocice[1], plocice[0]
-        print(plocice[0], plocice[1], plocice[2], plocice[3], plocice[4])
+        print(*plocice)
     #same for the second and third
     if plocice[1] > plocice[2]:
         plocice[1], plocice[2] = plocice[2], plocice[1]
-        print(plocice[0], plocice[1], plocice[2], plocice[3], plocice[4])
+        print(*plocice)
     #same for third and fourth
     if plocice[2] > plocice[3]:
         plocice[2], plocice[3] = plocice[3], plocice[2]
-        print(plocice[0], plocice[1], plocice[2], plocice[3], plocice[4])
+        print(*plocice)
     #same for fourth and fifth
     if plocice[3] > plocice[4]:
         plocice[3], plocice[4] = plocice[4], plocice[3]
-        print(plocice[0], plocice[1], plocice[2], plocice[3], plocice[4])
+        print(*plocice)
