f74e24aebd6db789df04ed95e1754ef08ae7493e
Dario Vuksan <65848142+IamMusavaRibica@users.noreply.github.com>
2024-11-05 10:33:29 +0100

    code style consistency

--- 9th class/PG6/TEKMA.py
+++ 9th class/PG6/TEKMA.py
@@ -1,25 +1,25 @@
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 z = 0
-if a + c == b + d:
+if a+c == b+d:
      
      e, f = map(int, input().split())
-     z= z + e + f
+     z += e + f
      
      if e == f:
           g, h = map(int, input().split())
-          z = z + g +h
-          if g>h:
-               print ('domacin')
-          elif g<h:
-               print ('gost')
-     elif e>f:
-          print ('domacin')
-     elif e<f:
+          z += g + h
+          if g > h:
+               print('domacin')
+          elif g < h:
+               print('gost')
+     elif e > f:
+          print('domacin')
+     elif e < f:
           print('gost')
-elif a+c>b+d:
+elif a+c > b+d:
      print('domacin')
-elif a+c<b+d:
+elif a+c < b+d:
      print('gost')
      
 print(a+c+b+d+z)
